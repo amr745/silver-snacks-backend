12f4fdeaa238226f8a2c059f89b7b6e7e1a7d554
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-07 18:10:42 -0700

    adds server listener

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+
+
+
 app.get('/', (req, res) => {
   res.status(200).json('root directory')
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is live on Port: ${PORT}`)
